8f63a162d42ccf2d7f8f0d3e1877cedf4e2d6805
OHsrio <Osvaldo_94@outlook.com>
2024-11-12 21:42:32 -0600

    Millas a Km

---
+++ MillasKm.py
@@ -0,0 +1,10 @@
+#1 milla equivale aproximadamente a 1.61 kilómetros# #
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
